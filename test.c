d4ca1bdfd728e4a12f3f772fdaf3f6d827347413
261705 <72786686+261705@users.noreply.github.com>
2021-04-06 11:44:30 +0530

    Add files via upload
    test file for complex calculator

---
+++ test.c
@@ -0,0 +1,19 @@
+/**
+ * @mainpage The testing of complex calculator is done here.
+ * This file contains the main() function for the entire project
+*/
+#include<stdio.h>
+#include"inc/header.h"
+int main(){
+    ///<Two complex numbers declared are going to be used for testing
+    complex num1,num2;
+    num1.real=3;
+    num1.img=4;
+    num2.real=5;
+    num2.img=6;
+    sum(num1,num2);
+    sqr(num1);
+    mul(num1,num2);
+    div(num1,num2);
+    return 0;
+}
